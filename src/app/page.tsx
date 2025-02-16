bd9593240956d29b5a7212d1d56cabc593485157
imran <33762887+imranprof@users.noreply.github.com>
2025-02-17 01:26:46 +0600

    chore: change default route branding to seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation"
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/branding");
+    router.push("/seo");
   }, [router]);
 
   return (
